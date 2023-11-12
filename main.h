fc19ab37c720358395b186df4cbcd4dc512cf30d
Morris Karimi <karimimorris08@gmail.com>
2023-11-12 06:20:58 -0800

    main

--- main.h
+++ main.h
@@ -39,4 +39,14 @@ typedef struct fmt fm_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[], int width, int precision, int size);
 
+/********** FUNCTIONS **********/
+
+/* Functions to print chars and strings */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int pritn_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int width, int size);
+
 #endif
