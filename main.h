583b217ceaf00118bd74ca59a86137816324438a
Morris Karimi <karimimorris08@gmail.com>
2023-11-12 06:41:18 -0800

    main

--- main.h
+++ main.h
@@ -56,5 +56,20 @@ int print_hexa_upper(va_list types, char buffer[], int flags, int width, int pre
 int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Functions to print non printable character */
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+/* Function to print memory address */
+int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+/* Function to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/* Function to print string in reverse */
+int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+/* width handler */
 
 #endif
