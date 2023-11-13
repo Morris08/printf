2a574a86830f5feb19326022807af07fc58a7146
Morris Karimi <karimimorris08@gmail.com>
2023-11-13 10:21:51 -0800

    handle

--- handle_print.c
+++ handle_print.c
@@ -16,7 +16,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 {
 	int i, unknow_len = 0, printed_char = -1;
 	fmt_t fmt_types[] = {
-	{'c', print char}, {'s', print_string}, {'%', print_percent}
+	{'c', print_char}, {'s', print_string}, {'%', print_percent},
+	{'i', print_int}, {'d', print_int}, {'b', print_binary},
+	{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+	{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+	{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[I].fmt != '\0'; i++)
 	if (fmt[*ind] == fmt_types[i].fmt)
